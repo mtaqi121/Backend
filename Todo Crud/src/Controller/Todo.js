9817ffb5d590e1ee7004bda815b89ff13cf13db3
mtaqi121 <mtaqibagla@gmail.com>
2025-09-04 08:34:53 -0700

    Update function added

--- Todo Crud/src/Controller/Todo.js
+++ Todo Crud/src/Controller/Todo.js
@@ -6,14 +6,26 @@ const addTodo = (req, res) => {
   return res.send({ massage: "Todo saved successfully" });
 };
 const deleteTodo = (req, res) => {
-    var id =req.params.id;
-   var filterTodos = todos.filter((obj) => obj.id != id)
-    todos = filterTodos;
-    return res.send ({message:"Todo Deleted Successfully"});
+  var id = req.params.id;
+  var filterTodos = todos.filter((obj) => obj.id != id);
+  todos = filterTodos;
+  return res.send({ message: "Todo Deleted Successfully" });
 };
 const getAllTodo = (req, res) => {
   return res.send({ todos, message: "All todos Fetched Successfully" });
 };
-const updateTodo = (req, res) => {};
+
+const updateTodo = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const { name, age } = req.body;
+
+  todos = todos.map((todo) => (todo.id === id ? { ...todo, name, age } : todo));
+
+  return res.send({
+    message: "Todo Updated Successfully",
+    todos,
+  });
+};
 
 export { addTodo, deleteTodo, getAllTodo, updateTodo };
--- Todo Crud/src/Router/Todo.js
+++ Todo Crud/src/Router/Todo.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { addTodo, deleteTodo, getAllTodo } from "../Controller/Todo.js";
+import { addTodo, deleteTodo, getAllTodo, updateTodo } from "../Controller/Todo.js";
 const router = express.Router();
 
 router.post("/create", addTodo);
 router.get("/get", getAllTodo);
 router.delete("/delete/:id", deleteTodo);
+router.put("/update/:id", updateTodo);
+
 export default router;  
